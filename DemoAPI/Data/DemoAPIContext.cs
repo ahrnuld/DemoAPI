bcf716fe63443710005fb29f99ec7ce82b0adfb1
Haan <Mark.deHaan@inholland.nl>
2020-10-03 19:46:05 +0200

    Add project files.

---
+++ DemoAPI/Data/DemoAPIContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoAPI.Models;
+
+namespace DemoAPI.Data
+{
+    public class DemoAPIContext : DbContext
+    {
+        public DemoAPIContext (DbContextOptions<DemoAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoAPI.Models.Product> Product { get; set; }
+
+        public DbSet<DemoAPI.Models.Category> Category { get; set; }
+    }
+}
